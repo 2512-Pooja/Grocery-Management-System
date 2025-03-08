689c522e4e6b994d58d74c62d40837fe1e3c33b1
Pooja <2512.poojasoni@gmail.com>
2025-03-08 23:00:44 +0530

    added docstring

--- src/main/java/com/grocery/org/OrgApplication.java
+++ src/main/java/com/grocery/org/OrgApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OrgApplication {
 
+	/**
+	 * The main method serves as the entry point for the Spring Boot application.
+	 * It uses SpringApplication.run to launch the application.
+	 *
+	 * @param args Command line arguments passed to the application.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(OrgApplication.class, args);
 		
